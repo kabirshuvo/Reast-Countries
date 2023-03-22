3a24602de320e4ff311d78e7f45320f12ef37fe0
kabirshuvo <kabirshuvo19@gmail.com>
2023-03-22 15:53:25 +0600

    My First React App is now live on Netlyfi

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -3,7 +3,11 @@ import './Country.css';
 
 const Country = (props) => {
     const {name, capital, flags, region, continents, currencies} = props.country;
-    // console.log(props.country)
+    let currency;
+    if(currencies){
+        currency = Object.keys(currencies)[0]
+        // console.log(currencies[currency].name);
+    }
     return (
         <div className='CountryDiv'>
             <div className='nameNcapital'>
@@ -13,7 +17,7 @@ const Country = (props) => {
             <img src={flags.png} alt="" />
             <div className='nameNcapital'>
                 <h2>{region}: <small>{continents}</small></h2>
-                {/* <h3>{currencies}</h3> */}
+                <h3>{currencies?.[currency]?.name}</h3>
             </div>
         </div>
     );
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='footer bg-success'>
+            <small>This is the footer @ All reight reaserved</small>
+        </div>
+};
+
+export default Footer;
