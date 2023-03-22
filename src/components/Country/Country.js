ed6d03c7f4cae86351058a1cf9eff236cc699618
kabirshuvo <kabirshuvo19@gmail.com>
2023-03-22 06:35:15 +0600

    Updated unique key

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './Country.css';
 
 const Country = (props) => {
-    const {name, capital, flags} = props.country;
-    console.log(props.country)
+    const {name, capital, flags, region, continents, currencies} = props.country;
+    // console.log(props.country)
     return (
         <div className='CountryDiv'>
             <div className='nameNcapital'>
@@ -12,8 +12,8 @@ const Country = (props) => {
             </div>
             <img src={flags.png} alt="" />
             <div className='nameNcapital'>
-                <h2>{name.common}</h2>
-                <h3>{capital}</h3>
+                <h2>{region}: <small>{continents}</small></h2>
+                {/* <h3>{currencies}</h3> */}
             </div>
         </div>
     );
